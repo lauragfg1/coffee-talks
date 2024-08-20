36c05a39676ddab8db38f0a1dd5cb11bbf0f3ead
lauragfg1 <lauragranda@uma.es>
2024-08-20 17:09:42 +0200

    User Random Button created and working properly.

--- src/main/java/com/example/germanTalks/controller/ChatController.java
+++ src/main/java/com/example/germanTalks/controller/ChatController.java
@@ -1,18 +1,35 @@
-package com.example.germanTalks.controller;
+/*package com.example.germanTalks.controller;
 
-import com.example.germanTalks.service.ChatGPTService;
+import org.hibernate.mapping.Map;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
-@RequestMapping("/api/chat")
 public class ChatController {
 
-    @Autowired
-    private ChatGPTService chatGPTService;
+    private final OllamaChatModel chatModel;
 
-    @PostMapping("/send")
-    public String getChatGPTResponse(@RequestBody String prompt) {
-        return chatGPTService.getChatGPTResponse(prompt);
+    @Autowired
+    public ChatController(OllamaChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    @GetMapping("/ai/generate")
+    public Map generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return Map.of("generation", chatModel.call(message));
+    }
+
+    @GetMapping("/ai/generateStream")
+    public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        Prompt prompt = new Prompt(new UserMessage(message));
+        return chatModel.stream(prompt);
     }
 }
+*/
