37c48d2a291cb524725de0112b64c110a3fa8a7a
lauragfg1 <lauragranda@uma.es>
2024-08-30 12:56:48 +0200

    Removed target

--- src/main/java/com/example/germanTalks/repository/TalkRepository.java
+++ src/main/java/com/example/germanTalks/repository/TalkRepository.java
@@ -1,7 +1,11 @@
 package com.example.germanTalks.repository;
 
 import com.example.germanTalks.model.Talk;
+import com.example.germanTalks.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TalkRepository extends JpaRepository<Talk, Integer> {
+    List<Talk> findByUser1(User user);
 }
